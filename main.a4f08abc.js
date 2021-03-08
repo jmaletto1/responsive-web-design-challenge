69bea5808370006562e7cdec02c85093f2e12688
Johnny Austen <johnnydausten@gmail.com>
2021-03-08 13:35:21 +0000

    Add parcel for building

---
+++ main.a4f08abc.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".collapsible").forEach((function(e){return e.addEventListener("click",(function(){this.classList.toggle("collapsible--expanded")}))}));
+//# sourceMappingURL=main.a4f08abc.js.map
