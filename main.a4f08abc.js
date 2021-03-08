87c0ead66b568c2c91e1f6f9d358bf5d2af6393f
Johnny Austen <johnnydausten@gmail.com>
2021-03-08 14:35:36 +0000

    dist

--- main.a4f08abc.js
+++
@@ -1,2 +0,0 @@
-document.querySelectorAll(".collapsible").forEach((function(e){return e.addEventListener("click",(function(){this.classList.toggle("collapsible--expanded")}))}));
-//# sourceMappingURL=main.a4f08abc.js.map
